1b3570e5ccb69dc50a34fcc722442e4ba82e7233
Rob Sawyer <robksawyer@gmail.com>
2017-02-26 02:33:17 -0800

    Getting a little further with this thing.

--- index.js
+++ index.js
@@ -9,77 +9,38 @@ function flatten(arr) {
 	return tmp.some(Array.isArray) ? flatten(tmp) : tmp;
 }
 
-// Handles getting the authorization code and refresh token
-// ENDPOINT
-// https://sso.digikey.com/as/authorization.oauth2
-// This endpoint is the target of the initial request. It handles authenticating the user and user consent.
-// The result includes the authorization code.
-//
-// PARAMETERS
-// response_type - code
-// Tells the Authorization Server to return an authorization code.
-//
-// client_id - This is the client id assigned to the application that you generated within the API Portal.
-// Identifies the client that is making the request. The value passed in this parameter must exactly match the value assigned by the API Portal.
-//
-// redirect_uri - This URI must match the redirect URI that you defined while creating your application within the API Portal.
-// Determines where the response is sent. The value of this parameter must exactly match the URI you provided while creating your application within the API Portal (including trailing '/').
-// function getAuthorizationCode(clientId, clientSecret, redirectUri, cb, responseType){
-// 	console.log('Running getAuthorizationCode()...');
-// 	if(!clientId) {
-// 		console.log('You must provide a DigiKey client id.');
-// 		return false;
-// 	}
-// 	if(!clientSecret) {
-// 		console.log('You must provide a DigiKey client secret.');
-// 		return false;
-// 	}
-// 	if(!redirectUri) {
-// 		console.log('You must provide a DigiKey redirect uri.');
-// 		return false;
-// 	}
-//
-// 	if(!responseType) {
-// 		responseType = 'code';
-// 	}
-// 	var opt = {
-// 		headers: {
-// 			accept: 'application/json',
-// 			'content-type': 'application/json'
-// 		},
-// 		url: url.format({
-// 			protocol: 'https',
-// 			host: 'sso.digikey.com',
-// 			pathname: 'https://sso.digikey.com/as/authorization.oauth2?response_type=' + responseType + '&client_id=' + clientId + '&redirect_uri=' + redirectUri
-// 		}),
-// 		json: true
-// 	};
-// 	console.log(opt);
-// 	return request.get(opt, cb ? function(err, res, body) {
-// 		console.log(err);
-// 		console.log(res);
-// 		console.log(body);
-// 		if (err)
-// 			cb(err);
-// 		else if (res.statusCode != 200)
-// 			cb(new Error(JSON.parse(body).message));
-// 		else
-// 			cb(null, JSON.parse(body));
-// 	} : null);
-// }
+function encodeFilters(filters) {
+	var result = flatten(Object.keys(filters).map(function(k) {
+		var v = Array.prototype.concat.apply([], filters[k]);
+		return v.map(function(vi) {
+			if (vi instanceof Object) {
+				return Object.keys(vi).map(function(ki) {
+					return k + '[' + ki + ']=' + querystring.escape(vi[ki]); // e.g. slice[field1]=1:5
+				});
+			} else {
+				return k + '[]=' + querystring.escape(vi); // e.g. show[]=foo
+			}
+		});
+	}));
+	return result;
+}
 
-var DigiKeyNode = function(clientId, clientSecret, redirectUri, apiVersion) {
+var DigiKeyNode = function(authToken, apiVersion) {
 	var self = this;
 
-	self.clientId = (clientId) ? clientId : process.env.DIGIKEY_CLIENT_ID;
-	self.clientSecret = (clientSecret) ? clientSecret : process.env.DIGIKEY_CLIENT_SECRET;
-	self.redirectUri = redirectUri;
+	self.clientId = process.env.DIGIKEY_CLIENT_ID;
+	self.clientSecret = process.env.DIGIKEY_CLIENT_SECRET;
+	self.redirectUri = process.env.DIGIKEY_CALLBACK_URL;
 	self.apiVersion = (apiVersion) ? apiVersion : 'v1';
+	self.authToken = authToken;
 
-	if(!redirectUri){
-		if(process.env.DIGIKEY_CALLBACK_URL){
-			self.redirectUri = process.env.DIGIKEY_CALLBACK_URL;
-		}
+	if(!self.authToken){
+		console.error('You must provide a valid authorization token. See https://api-portal.digikey.com/start for more.');
+		return new Error('You must provide a valid authorization token. See https://api-portal.digikey.com/start for more.');
+	}
+
+	// Try an alternate env variable
+	if(!self.redirectUri){
 		if(process.env.DIGIKEY_REDIRECT_URI){
 			self.redirectUri = process.env.DIGIKEY_REDIRECT_URI;
 		}
@@ -87,32 +48,52 @@ var DigiKeyNode = function(clientId, clientSecret, redirectUri, apiVersion) {
 
 	self.country = 'US'; // See ISO Code http://www.nationsonline.org/oneworld/country_code_list.htm
 	self.lang = 'en'; //"en", "de", "fr", "ko", "zhs", "zht", "it", "es", "he"
-	self.locale = '';
+	self.locale = 'us'; // "ad", "ae", "af", "ag", "ag", "ai", "al", "am", "an", "ao", "aq", "ar", "as",
+											// "at", "au", "aw", "ax", "az", "ba", "bb", "bd", "be", "bf", "bg", "bh", "bi",
+											// "bj", "bm", "bn", "bo", "br", "bs", "bt", "bv", "bw", "by", "bz", "ca", "cc",
+											// "cd", "cf", "cg", "ch", "ci", "ck", "cl", "cm", "cn", "co", "cr", "cs", "cu",
+											// "cv", "cw", "cx", "cy", "cz", "de", "dj", "dk", "dm", "do", "dz", "ec", "ee",
+											// "eg", "eh", "er", "es", "et", "fi", "fj", "fk", "fm", "fo", "fr", "ga", "gb",
+											// "gd", "ge", "gf", "gh", "gi", "gl", "gm", "gn", "gp", "gq", "gr", "gs", "gt",
+											// "gu", "gw", "gy", "hk", "hm", "hn", "hr", "ht", "hu", "id", "ie", "il", "in",
+											// "io", "iq", "ir", "is", "it", "jm", "jo", "jp", "ke", "kg", "kh", "ki", "km",
+											// "kn", "kp", "kr", "kw", "ky", "kz", "la", "lb", "lc", "li", "lk", "lr", "ls",
+											// "lt", "lu", "lv", "ly", "ma", "mc", "md", "me", "mg", "mh", "mk", "ml", "mm",
+											// "mn", "mo", "mp", "mq", "mr", "ms", "mt", "mu", "mv", "mw", "mx", "my", "mz",
+											// "na", "nc", "ne", "nf", "ng", "ni", "nl", "no", "np", "nr", "nu", "nz", "om",
+											// "pa", "pe", "pf", "pg", "ph", "pk", "pl", "pm", "pn", "pr", "ps", "pt", "pw",
+											// "py", "qa", "re", "ro", "rs", "ru", "rw", "sa", "sb", "sc", "sd", "se", "sg",
+											// "sh", "si", "sj", "sk", "sl", "sm", "sn", "so", "sr", "st", "sv", "sx", "sy",
+											// "sz", "tc", "td", "tf", "tg", "th", "tj", "tk", "tl", "tm", "tn", "to", "tp",
+											// "tr", "tt", "tv", "tw", "tz", "ua", "ug", "uy", "um", "us", "uz", "va", "vc",
+											// "ve", "vg", "vi", "vn", "vu", "wf", "ws", "ye", "yt", "yu", "za", "zm", "zw"
 	self.currency = 'usd'; //"usd", "cad", "jpy", "gbp", "eur", "hkd", "sgd", "twd", "krw", "aud", "nzd", "inr", "dkk", "nok", "sek", "ils"
 
-	//https://api.digikey.com/services/keywordsearch/v1/search
+	// Valid Endpoints
+	// https://api.digikey.com/services/keywordsearch/v1/search
+	// https://api.digikey.com/services/basicsearch/v1/search
 	var send = function(service, path, params, filters, cb) {
 		if (typeof filters === 'function') {
 			cb = filters; // skip filters
 		} else if (filters) {
-			// params = params.concat(encodeFilters(filters));
+			params = params.concat(encodeFilters(filters));
 		}
 		var opt = {
 			method: 'POST',
 			headers: {
 				'x-digikey-locale-shiptocountry': self.country,
 				'x-digikey-locale-currency': self.currency,
-				// 'x-digikey-locale-site': self.locale,
+				'x-digikey-locale-site': self.locale,
 				'x-digikey-locale-language': self.lang,
-				authorization: '',
+				authorization: self.authToken,
 				accept: 'application/json',
 				'content-type': 'application/json',
-				'x-ibm-client-id': 'REPLACE_THIS_KEY'
+				'x-ibm-client-id': self.clientId
 			},
 			url: url.format({
 				protocol: 'https',
 				host: 'api.digikey.com',
-				pathname: '/services/' + service + '/api/' + self.apiVersion + '/' + path
+				pathname: '/services/' + service + '/' + self.apiVersion + '/' + path
 			}),
 			body: {
 				PartPreference: 'CT', Quantity: 25, PartNumber: 'P5555-ND'
@@ -120,6 +101,9 @@ var DigiKeyNode = function(clientId, clientSecret, redirectUri, apiVersion) {
 			json: true
 		};
 		return request.get(opt, cb ? function(err, res, body) {
+			console.log(err);
+			console.log(res);
+			console.log(body);
 			if (err)
 				cb(err);
 			else if (res.statusCode != 200)
@@ -127,10 +111,6 @@ var DigiKeyNode = function(clientId, clientSecret, redirectUri, apiVersion) {
 			else
 				cb(null, JSON.parse(body));
 		} : null);
-	};
-
-	self.getAuthCode = function(cb) {
-		return getAuthorizationCode(self.clientId, self.clientSecret, self.redirectUri, cb);
 	};
 
 	['basic', 'keyword'].forEach(function(name) {
@@ -151,7 +131,7 @@ var DigiKeyNode = function(clientId, clientSecret, redirectUri, apiVersion) {
 			var params = [].concat(args).map(function(key) {
 				return querystring.stringify(key);
 			});
-			return send(name + '/search', params, filters, cb);
+			return send(name + 'search', 'search', params, filters, cb);
 		};
 	});
 
@@ -159,8 +139,6 @@ var DigiKeyNode = function(clientId, clientSecret, redirectUri, apiVersion) {
 };
 
 // https://sso.digikey.com/as/authorization.oauth2?response_type=code&client_id=123456789abcdefg&redirect_uri=https://my-new-app.example.com/code
-exports.createV1 = function(clientId, clientSecret, redirectUri) {
-	console.log('Inside of createV1');
-
-	return new DigiKeyNode(clientId, clientSecret, redirectUri, 'v1');
+exports.createV1 = function(authToken) {
+	return new DigiKeyNode(authToken, 'v1');
 };
--- tests/apiSpec.js
+++ tests/apiSpec.js
@@ -1,4 +1,4 @@
-var assert = require('assert');
+var assert = require('chai').assert;
 var sinon = require('sinon');
 var PassThrough = require('stream').PassThrough;
 var http = require('http');
@@ -7,10 +7,11 @@ var request = require('request');
 require('dotenv').config();
 require('request-debug')(request);
 
-var app = require('../app.js');
 var api = require('../index.js');
 
-describe('api', function() {
+var cli = undefined;
+
+describe('DigiKey Api', function() {
 	beforeEach(function() {
 		this.request = sinon.stub(http, 'request');
 	});
@@ -20,18 +21,19 @@ describe('api', function() {
 	});
 
 	//We will place our tests cases here
-	it('should convert get result to object', function(done) {
-		var expected = {
-			"access_token": "weoiaslkjfoiw32/12#kd",
-			"refresh_token": "aslidf390-sdl/jliDLSksli",
-			"token_type": "Bearer",
-			"expires_in": 3920
-		};
-		var response = new PassThrough();
-		response.write(JSON.stringify(expected));
-		response.end();
-
-		var request = new PassThrough();
+	it('should create an instance of the cli', function(done) {
+		// var expected = {
+		// 	"access_token": "weoiaslkjfoiw32/12#kd",
+		// 	"refresh_token": "aslidf390-sdl/jliDLSksli",
+		// 	"token_type": "Bearer",
+		// 	"expires_in": 2592000,
+		// 	"expires_at": 2592000
+		// };
+		// var response = new PassThrough();
+		// response.write(JSON.stringify(expected));
+		// response.end();
+		//
+		// var request = new PassThrough();
 
 		// this.request
 		// 	.callsArgWith(1, response)
@@ -41,13 +43,27 @@ describe('api', function() {
 		// Error: https://my-new-app.example.com/code?error=access_denied
 		// Success: https://my-new-app.example.com/code?code=6513183215H5465sdlkjKils
 
-		var cli = app.createV1(process.env.DIGIKEY_CLIENT_ID, process.env.DIGIKEY_CLIENT_SECRET, process.env.DIGIKEY_CALLBACK_URL);
-		cli.getAuthCode(function(err, results){
-			console.log(err);
+		//NOTE: In order to use the tests. You'll need to generate your own authorization token.
+		cli = api.createV1(process.env.DIGIKEY_AUTH_TOKEN);
+		if(!process.env.DIGIKEY_AUTH_TOKEN){
+			var err = 'You must provide a valid authorization token. See https://api-portal.digikey.com/start for more.';
+			done(err);
+		}
+		assert.isObject(cli, 'cli is an object');
+		done();
+	});
+
+	//We will place our tests cases here
+	it('should get search results', function(done) {
+		// args, filters, callback
+		cli.basicSearch({},{},function(err, results){
 			console.log(results);
+			if(err){
+				done(err);
+			}
+			assert.isAbove(results, 0, 'result length above 0');
 			done();
 		});
-
 	});
 
 	// it('should send post params in request body', function() {
@@ -64,17 +80,4 @@ describe('api', function() {
 	// 	assert(write.withArgs(expected).calledOnce);
 	// });
 
-	it('should pass request error to callback', function(done) {
-		var expected = 'some error';
-		var request = new PassThrough();
-
-		this.request.returns(request);
-
-		api.auth(function(err) {
-			assert.equal(err, expected);
-			done();
-		});
-
-		request.emit('error', expected);
-	});
 });
